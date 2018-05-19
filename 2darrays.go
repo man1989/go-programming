9855fb7aa367644bd79b914b1950cc6cac6385b8
Manish Kumar <manish.sep1989@gmail.com>
2018-05-19 07:05:46 +0530

    arrays in go

---
+++ 2darrays.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var arr [10]int
+	var _2dArr [5][5]int
+	arr = append(arr, 1, 2, 3)
+	fmt.Println(arr)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			_2dArr[i][j] = i + j
+		}
+	}
+	fmt.Println("2d array: ", _2dArr)
+}
