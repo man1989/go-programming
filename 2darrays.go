523effe5cb37d5a0c34b0abd29128032f75023de
Manish <manish@localhost.localdomain>
2018-05-22 12:00:39 +0530

    append for slices

--- 2darrays.go
+++ 2darrays.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	var arr [10]int
 	var _2dArr [5][5]int
-	arr = append(arr, 1, 2, 3)
+	// arr = append(arr, 1, 2, 3)
 	fmt.Println(arr)
 	for i := 0; i < 5; i++ {
 		for j := 0; j < 5; j++ {
